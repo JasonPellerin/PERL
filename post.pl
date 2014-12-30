96f460f7f3aaa797a2b63e419df8e4c2793afbd2
JasonPellerin <pellerinj5@gmail.com>
2014-12-30 14:06:24 -0800

    2nd commit

---
+++ post.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+$|=1;
+
+ sub main{
+       my $file = '/var/log/apache2/access.log';
+       
+       open(INPUT, $file) or die("Whatchu talkin bout willis?\n");
+       
+       while(my $line = <INPUT>) {
+       	if($line =~ /POST/) {
+       		print $line;
+       	}
+       }
+       close(INPUT);
+ }
+ main();
